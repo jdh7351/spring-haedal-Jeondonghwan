b2f73d9404933b2042330cda5dd1fcdf686e0d12
jdh7351 <jdh7351@naver.com>
2024-04-03 16:01:33 +0900

    6. POST API /multiply 만들어보기

--- library-app/library-app/src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ library-app/library-app/src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,7 +1,10 @@
 package com.group.libraryapp.controller.calculator;
 
 import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
+import com.group.libraryapp.dto.calculator.request.CalculatorMultiplyRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,4 +16,10 @@ public class CalculatorController {
     public int addTwoNumbers(CalculatorAddRequest request){
         return request.getNumber1() + request.getNumber2();
     }
+
+    @PostMapping("/multiply") // Post /multiply
+    public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request){
+        return request.getNumber1() * request.getNumber2();
+    }
+
 }
