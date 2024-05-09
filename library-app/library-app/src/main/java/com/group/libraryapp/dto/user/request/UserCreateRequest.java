ee62e5e15166e682611acc66ff86d5f6dd3b4166
jdh7351 <jdh7351@naver.com>
2024-05-09 19:24:41 +0900

    chapter 2. DB 사용하기

--- library-app/library-app/src/main/java/com/group/libraryapp/dto/user/request/UserCreateRequest.java
+++ library-app/library-app/src/main/java/com/group/libraryapp/dto/user/request/UserCreateRequest.java
@@ -3,7 +3,7 @@ package com.group.libraryapp.dto.user.request;
 public class UserCreateRequest {
 
     private String name;
-    private Integer age; // Ingeger => NULL 표시가능(int는 불가능)
+    private Integer age; // Integer => NULL 표시가능(int는 불가능)
 
     public String getName() {
         return name;
