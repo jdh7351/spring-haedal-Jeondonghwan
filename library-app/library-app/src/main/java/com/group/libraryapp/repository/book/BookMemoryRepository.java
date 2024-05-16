fd2b9cf1f1e9632d93a31fff0cde27ef7ed5175c
jdh7351 <jdh7351@naver.com>
2024-05-16 19:01:50 +0900

    chapter 3.Clean_Code

---
+++ library-app/library-app/src/main/java/com/group/libraryapp/repository/book/BookMemoryRepository.java
@@ -0,0 +1,15 @@
+package com.group.libraryapp.repository.book;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class BookMemoryRepository implements BookRepository{
+
+    @Override
+    public void saveBook(){
+
+    }
+}
